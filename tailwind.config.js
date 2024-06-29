5beb38e45a38cadcb0ce2faad6a4da2f9919f62a
rohitsensai <satputerohit00@gmail.com>
2024-06-29 18:01:39 +0530

    new commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./node_modules/flowbite-react/**/*.js",
-    "./node_modules/flowbite/**/*.js",
+
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
