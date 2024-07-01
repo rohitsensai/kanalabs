b9ec33078dc6b6b98cdca3ecaa963709b5e344d9
rohitsensai <satputerohit00@gmail.com>
2024-07-01 09:52:18 +0530

    styled icons

--- components/main.js
+++ components/main.js
@@ -19,7 +19,7 @@ export default function main() {
 
                     <div className='d-flex flex-row' style={{ fontWeight: "800", fontSize: "12px" }}>
 
-                        <div className='d-flex flex-row px-2 rounded text-white py-2' style={{ border: "1px solid #ffffff", backgroundColor: "#17181A", }}>
+                        <div className='d-flex flex-row px-2 rounded text-white py-2' style={{ border: "1px solid #ffffff", backgroundColor: "#17181A" }}>
                             <TbArrowsSplit2 className='mx-1 mb-1' />
                             Same-chain
                         </div>
@@ -139,15 +139,15 @@ export default function main() {
 
 
 
-            <div className='d-block d-sm-none ' style={{ backgroundColor: "#0C0C0D", height: "110vh", fontWeight: "800", fontSize: "14px" }}>
+            <div className='d-block d-sm-none ' style={{ backgroundColor: "#0C0C0D", height: "105vh", fontWeight: "800", fontSize: "14px" }}>
                 <div className="w-full mx-auto   rounded" style={{ height: "450px" }}>
 
                     <div className='d-flex text-white'>
-                        <div className=' text-center rounded  text-white flex-grow-1 py-3'>
+                        <div className='d-flex flex-row align-items-center justify-content-center  text-center rounded  text-white flex-grow-1 py-3'>
                             <TbArrowsSplit2 className='' />
                             Same-Chain
                         </div>
-                        <div className='me-auto text-center rounded flex-grow-1 py-3' style={{ backgroundColor: "#111213" }}>
+                        <div className='me-auto d-flex flex-row align-items-center justify-content-center  text-center rounded flex-grow-1 py-3' style={{ backgroundColor: "#111213" }}>
                             <TiArrowShuffle className='' />
                             Cross-Chain
                         </div>
@@ -172,12 +172,12 @@ export default function main() {
 
                         <div className='d-flex text-white px-2 pb-2'>
 
-                            <img src='https://s3-alpha-sig.figma.com/img/e909/bd74/aa0a9be6883d6cde025f2ba30e1038a9?Expires=1720396800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=jYE3YM3TrSeDo4Gz1EKQv6W1z6RBjAHx27nqDqE-AZb3dZ5jIL~PcTKWIrBUY2CjzCwY906xk6YozUnRZjZwVEjup8ZfVZCC9bdvubr91N74SZdwgzecePPKdfRPyZ4dBJc-nozL22WWQLrt6IaCd-I-CANaTBCz3uxcm8fwKCJmTrl8Sf18E~ST-kGN9hTFjfvA1PGlEZPI7d2o7azS5aZoE2-HTlBsolb7qjkD7kP9cLG1IsS5kEn0e6bbx0q-YMAzqRYa81ZZd63pBITUMGgmMBzhcWl0kYZPNzgP5Ig5rS1pYHRsWcVKzgoHgDgE6E-zE8CeGj~iwjDfnXg6pQ__' width={24} height={24} className='mx-2' />
-                            <img src='https://s3-alpha-sig.figma.com/img/8a6c/ae54/b45e3da7af6e8af4a4c440376b72b965?Expires=1720396800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=EI~uMznzXE8Nks-AXTA~W5l6gLVpiqg0xQZ8iBHg0iQIuf4xLgclW~qTv7U-St6FjbgZibZ93IyapDdSjczJ3PKSdn499tQjvbKAMFfE86gBpYBj~1BEDLu7dH2OeJZUfKA0Q4eyRZVicGB2zSwtmEntP9BESoujdXwYP1gwHRyHxhV3I9FUHS6MqMF2ss76bBwoSdZrrS1yInAH2XeMdbHRpQp440ULrw6u6~vddgoHky8P6cHZoWkGd7nwMAqJqkt25pJC4aHUWmmvSr-S~LR4nKzqPf2JFDzZoW16nWko~-Ja0S5NJDQnHtn2t3U-7R3Dbu~1~TVESbZd~05rlA__' width={24} height={24} className='mx-2' />
-                            <img src='https://s3-alpha-sig.figma.com/img/1232/5c5c/aed6729c431959cbcbfe061a43ae4ebf?Expires=1720396800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=co-gKUdImDw8aB7D9TzcVi8Mb8ZTVhzmgazsNDCDfDFFPmuSIY1MDUyY38Zzavxq9aMEiRETqEFHqVQq2X4HpyZ8xLSwvzjGuKau67ZgM6lD2DKXSlrisVBHgH5EL4G84tvBegR00Fo-LJMlzmr1HMNCufTQ6-4ETFQkv-CefXMWHfP9eK4FssxlVfKAHvhkjQAxlBm5F76EWoyUvSC3ckfOdLMoWEV5BVJdqb2WxbO1eLfuwwi0xgVfXPgCMnLp8-TaqoCN81PDHYzTSRmu2OL4HxzPG2qSXO~XtYlaP~GflNekJ4o0s11KifPQCwdax40D6OkhvZYTOEdntsCQvA__' width={24} height={24} className='mx-2' />
-                            <img src='https://s3-alpha-sig.figma.com/img/3ec3/9070/cfea798a13336af431617b531e74fc20?Expires=1720396800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=hFLIcOoH7IDhAsNE5~IKa5fEZUKP~Qu-JnNrDXMvyXesPhIUSCE1GtOZJoX7l4lI9xt7d2Sr1LEoiZytdi7TQsmkuxw6CLp0xkqLAHKCnsFaed5k3k3R7tIZg6On3TOu5GX-n-CeoxoUhFewrWQSiTvhmV7NYM6ePRAPpKc3U8sGpanU-qlu5lNpOOweIhS0FVKk1V06fO0l8B85XXl8cYxbo~0XiyjljM0KHMOqAuM-5fRqhly27RstBUb0XTBPmFcJDd~sAS-ElwjkoJaeJaZSo6MvsstCPPb3YNZuzLsOVlganUbmV9t57h8ThVLuXODtQTUxxrKSpDDTM6DgbQ__' width={24} height={24} className='me-auto mx-2' />
+                            <img src='https://s3-alpha-sig.figma.com/img/e909/bd74/aa0a9be6883d6cde025f2ba30e1038a9?Expires=1720396800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=jYE3YM3TrSeDo4Gz1EKQv6W1z6RBjAHx27nqDqE-AZb3dZ5jIL~PcTKWIrBUY2CjzCwY906xk6YozUnRZjZwVEjup8ZfVZCC9bdvubr91N74SZdwgzecePPKdfRPyZ4dBJc-nozL22WWQLrt6IaCd-I-CANaTBCz3uxcm8fwKCJmTrl8Sf18E~ST-kGN9hTFjfvA1PGlEZPI7d2o7azS5aZoE2-HTlBsolb7qjkD7kP9cLG1IsS5kEn0e6bbx0q-YMAzqRYa81ZZd63pBITUMGgmMBzhcWl0kYZPNzgP5Ig5rS1pYHRsWcVKzgoHgDgE6E-zE8CeGj~iwjDfnXg6pQ__' style={{width:"24px",height:"24px",objectFit:"cover"}} className='mx-2' />
+                            <img src='https://s3-alpha-sig.figma.com/img/8a6c/ae54/b45e3da7af6e8af4a4c440376b72b965?Expires=1720396800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=EI~uMznzXE8Nks-AXTA~W5l6gLVpiqg0xQZ8iBHg0iQIuf4xLgclW~qTv7U-St6FjbgZibZ93IyapDdSjczJ3PKSdn499tQjvbKAMFfE86gBpYBj~1BEDLu7dH2OeJZUfKA0Q4eyRZVicGB2zSwtmEntP9BESoujdXwYP1gwHRyHxhV3I9FUHS6MqMF2ss76bBwoSdZrrS1yInAH2XeMdbHRpQp440ULrw6u6~vddgoHky8P6cHZoWkGd7nwMAqJqkt25pJC4aHUWmmvSr-S~LR4nKzqPf2JFDzZoW16nWko~-Ja0S5NJDQnHtn2t3U-7R3Dbu~1~TVESbZd~05rlA__' style={{width:"24px",height:"24px",objectFit:"cover"}} className='mx-2' />
+                            <img src='https://s3-alpha-sig.figma.com/img/1232/5c5c/aed6729c431959cbcbfe061a43ae4ebf?Expires=1720396800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=co-gKUdImDw8aB7D9TzcVi8Mb8ZTVhzmgazsNDCDfDFFPmuSIY1MDUyY38Zzavxq9aMEiRETqEFHqVQq2X4HpyZ8xLSwvzjGuKau67ZgM6lD2DKXSlrisVBHgH5EL4G84tvBegR00Fo-LJMlzmr1HMNCufTQ6-4ETFQkv-CefXMWHfP9eK4FssxlVfKAHvhkjQAxlBm5F76EWoyUvSC3ckfOdLMoWEV5BVJdqb2WxbO1eLfuwwi0xgVfXPgCMnLp8-TaqoCN81PDHYzTSRmu2OL4HxzPG2qSXO~XtYlaP~GflNekJ4o0s11KifPQCwdax40D6OkhvZYTOEdntsCQvA__' style={{width:"24px",height:"24px",objectFit:"cover"}} className='mx-2' />
+                            <img src='https://s3-alpha-sig.figma.com/img/3ec3/9070/cfea798a13336af431617b531e74fc20?Expires=1720396800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=hFLIcOoH7IDhAsNE5~IKa5fEZUKP~Qu-JnNrDXMvyXesPhIUSCE1GtOZJoX7l4lI9xt7d2Sr1LEoiZytdi7TQsmkuxw6CLp0xkqLAHKCnsFaed5k3k3R7tIZg6On3TOu5GX-n-CeoxoUhFewrWQSiTvhmV7NYM6ePRAPpKc3U8sGpanU-qlu5lNpOOweIhS0FVKk1V06fO0l8B85XXl8cYxbo~0XiyjljM0KHMOqAuM-5fRqhly27RstBUb0XTBPmFcJDd~sAS-ElwjkoJaeJaZSo6MvsstCPPb3YNZuzLsOVlganUbmV9t57h8ThVLuXODtQTUxxrKSpDDTM6DgbQ__' style={{width:"24px",height:"24px",objectFit:"cover"}} className='me-auto mx-2' />
 
-                            <div className='rounded shadow py-1 px-1' style={{ color: "#27e3c4", border: "1px solid #27e3c4" }}>Other Chains <FaChevronDown /> </div>
+                            <div className='rounded shadow px-2' style={{ width:"35%",color: "#27e3c4", border: "1px solid #27e3c4" }}>Other Chains <FaChevronDown /> </div>
                         </div>
 
 
@@ -273,11 +273,11 @@ export default function main() {
                             More Routes <FaChevronDown className='mt-1 mx-2' />
                         </div>
 
-                        <div className='d-flex mx-4 my-4 py-1 rounded shadow bg-dark justify-content-center cursor-pointer ' style={{ color: "#2ED3B7", border: "1px solid #2ED3B7", borderBottom: "4px solid #2ED3B7" ,fontWeight:"800",fontSize:"16px"}}>
+                        <div className='d-flex mx-2 my-4 py-3 rounded shadow bg-dark justify-content-center cursor-pointer ' style={{ color: "#2ED3B7", border: "1px solid #2ED3B7", borderBottom: "4px solid #2ED3B7" ,fontWeight:"800",fontSize:"16px"}}>
                             Swap now
                         </div>
 
-                        <div className='mx-4 px-2 my-4 py-1 shadow rounded d-flex justify-content-center cursor-pointer text-white ' style={{ border: "1px solid #919191", backgroundColor: "#17181A" }}>
+                        <div className='mx-2 px-4 my-4 py-3 shadow rounded d-flex justify-content-center cursor-pointer text-white ' style={{ border: "1px solid #919191", backgroundColor: "#17181A" }}>
                             <div className='me-auto' style={{ backgroundColor: "#17181A", fontWeight: "400", fontSize: "14px" }}>
                                 Additional details
                             </div>
